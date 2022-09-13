719aad248f17d22d30ae8b866eb6101cf677dedd
蒋东旗 <jdq8576@github.com>
2022-09-13 15:16:17 +0800

    something modify

--- src/main/java/cn/edu/njtech/config/RedisConfig.java
+++ src/main/java/cn/edu/njtech/config/RedisConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.Serializable;
 
@@ -21,25 +19,13 @@ import java.io.Serializable;
 @Configuration
 public class RedisConfig {
 
-    @Bean(name = "jedisPoolConfig")
-    public JedisPoolConfig getJedisPoolConfig() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(300);
-        jedisPoolConfig.setMaxWaitMillis(10000);
-        jedisPoolConfig.setMaxIdle(100);
-        return jedisPoolConfig;
-    }
 
     @Bean(name = "jedisConnectionFactory")
-    public JedisConnectionFactory getJedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+    public JedisConnectionFactory getJedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName("127.0.0.1");
         redisStandaloneConfiguration.setPort(6379);
         redisStandaloneConfiguration.setDatabase(0);
-        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jedisPoolConfigBuilder =
-                (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
-        jedisPoolConfigBuilder.poolConfig(jedisPoolConfig);
-
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
         return jedisConnectionFactory;
     }
@@ -56,7 +42,7 @@ public class RedisConfig {
         // key的序列化采用StringRedisSerializer
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-
+        
         template.setConnectionFactory(jedisConnectionFactory);
         return template;
     }
